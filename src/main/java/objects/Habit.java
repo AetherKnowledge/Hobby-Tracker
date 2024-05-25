ebbc4e58ccc42062b2b5eea3385f38db471ca972
AetherKnowledge <152044019+AetherKnowledge@users.noreply.github.com>
2024-05-26 03:40:07 +0800

    Functionality - Habit Class

--- src/main/java/objects/Habit.java
+++ src/main/java/objects/Habit.java
@@ -1,15 +1,51 @@
 
 package objects;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class Habit {
     
-//    private final String name;
-//    private final boolean isTimed;
-//    private final IconType iconType;
-//    private final duration;
-//    private final ammount;
+    private final String name;
+    private final IconType iconType;
+    private final ArrayList<DayOfWeek> daysOfWeek;
+    private final LocalTime timeStart;
+    private final int durationSeconds;
+    private final int ammount;
     
-    
+    public Habit(String name, IconType iconType, ArrayList<DayOfWeek> daysOfWeek, LocalTime timeStart, int durationSeconds, int ammount){
+        this.name = name;
+        this.iconType = iconType;
+        this.daysOfWeek = daysOfWeek;
+        this.timeStart = timeStart;
+        this.durationSeconds = durationSeconds;
+        this.ammount = ammount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public IconType getIconType() {
+        return iconType;
+    }
+
+    public ArrayList<DayOfWeek> getDaysOfWeek() {
+        return daysOfWeek;
+    }
+
+    public LocalTime getTimeStart() {
+        return timeStart;
+    }
+
+    public int getDurationSeconds() {
+        return durationSeconds;
+    }
+
+    public int getAmmount() {
+        return ammount;
+    }
     
     public enum IconType{
         RUNNING, MEDITATE;
